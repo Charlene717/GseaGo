6cd2d046a1294f4359995724c8d2860cda8c3e73
CharleneMSI <q2330416@gmail.com>
2022-05-31 16:33:35 +0800

    Export PDF.

--- GSEA_Analysis/GSEA_Analysis_Example_fgsea_Visu.R
+++ GSEA_Analysis/GSEA_Analysis_Example_fgsea_Visu.R
@@ -14,9 +14,27 @@
   rm(list = ls()) # Clean variable
   memory.limit(150000)
 
+##### Current path and new folder setting* #####
+  ProjectName = "Example"
+  Sampletype = "Mm"
+  #ProjSamp.Path = paste0(Sampletype,"_",ProjectName)
+
+  Version = paste0(Sys.Date(),"_",ProjectName,"_",Sampletype)
+  Save.Path = paste0(getwd(),"/",Version)
+  ## Create new folder
+  if (!dir.exists(Save.Path)){
+    dir.create(Save.Path)
+  }
+
+  ## Import information
+  InputFolder = "Input_files_10x"
+  InputAnno = "PBMC_Ano.csv"
+
+  InputGSEA = "GSEA_Geneset_Pathway_3Database_WithoutFilter.txt"
+
 ##### Parameter setting* #####
   # Set the desired organism
-  organism = "org.Dm.eg.db"
+  organism = "org.Mm.eg.db" # organism = "org.Hs.eg.db", "org.Mm.eg.db", "org.Dm.eg.db"
 
 ##### Load Packages  #####
   #### Basic installation ####
@@ -58,7 +76,6 @@
   library(ggstance)
 
 
-
 ##### Load Files #####
   ## Load data
   setwd("../")
@@ -81,13 +98,19 @@
     # Plot the ranked fold changes.
     barplot(sort(ranks, decreasing = T))
 
+    pdf(file = paste0(Save.Path,"/",ProjectName,"_Rankbarplot.pdf"),
+        width = 17,  height = 7
+    )
+    barplot(sort(ranks, decreasing = T))
+    dev.off()
+
 
   #### Conduct analysis ####
-    fgseaRes <- fgsea(pathwaysH, ranks, minSize=15, maxSize = 500, nperm=1000)
-    ## fgsea: What does fgseaMultilevel argument sampleSize mean/when to change it?
-    ## https://www.biostars.org/p/479821/
+    # fgseaRes <- fgsea(pathwaysH, ranks, minSize=15, maxSize = 500, nperm=1000)
+    # ## fgsea: What does fgseaMultilevel argument sampleSize mean/when to change it?
+    # ## https://www.biostars.org/p/479821/
 
-    fgseaRes <- fgseaMultilevel(pathwaysH, ranks, minSize=15, maxSize = 500)
+    fgseaRes <- fgseaMultilevel(pathwaysH, ranks, minSize=15, maxSize = 500, nPermSimple = 1000)
     ## Error when running parallelized process: Warning in serialize... package:stats may not be available when loading
     ## https://community.rstudio.com/t/error-when-running-parallelized-process-warning-in-serialize-package-stats-may-not-be-available-when-loading/110573
     ## https://stackoverflow.com/questions/27623901/r-warning-packagestats-may-not-be-available-when-loading
@@ -107,6 +130,15 @@
       top_n(10, wt=-padj)
     topPathways <- bind_rows(topUp, topDown) %>%
       arrange(-ES)
+    plotGseaTable(pathwaysH[topPathways$pathway],
+                  ranks,
+                  fgseaRes,
+                  gseaParam = 0.5)
+    dev.off()
+
+    pdf(file = paste0(Save.Path,"/",ProjectName,"_tableplot.pdf"),
+        width = 12,  height = 7
+    )
     plotGseaTable(pathwaysH[topPathways$pathway],
                   ranks,
                   fgseaRes,
@@ -138,37 +170,45 @@
     library(ggplot2)
 
     n <- 10
-    y_bar <- group_by(y, sign(NES)) %>%
-      slice(1:n)
+    y_bar <- group_by(y, sign(NES)) %>% slice(1:n)
 
     ggplot(y_bar, aes(NES, fct_reorder(Description, NES), fill = qvalues), showCategory=(n*2)) +
-      geom_barh(stat='identity') +
-      scale_fill_continuous(low='red', high='blue', guide=guide_colorbar(reverse=TRUE)) +
-      theme_minimal() + ylab(NULL)
+           geom_barh(stat='identity') +
+           scale_fill_continuous(low='red', high='blue', guide=guide_colorbar(reverse=TRUE)) +
+           theme_minimal() + ylab(NULL) -> P2_1.Barplot
+    P2_1.Barplot
 
     ## 2.2 Dotplot
-    dotplot(msC2_2, showCategory = 10, font.size = 8,
-            x = "GeneRatio",   # option -> c("GeneRatio", "Count")
-            color = "p.adjust")   # option -> c("pvalue", "p.adjust", "qvalue")
+    P2_2.Dotplot <- dotplot(msC2_2, showCategory = 10, font.size = 8,
+                            x = "GeneRatio",   # option -> c("GeneRatio", "Count")
+                            color = "p.adjust")   # option -> c("pvalue", "p.adjust", "qvalue")
+    P2_2.Dotplot
 
     ## 2.3 Gene-Concept Network
     n <- 3
     p1 <- cnetplot(msC2_2, showCategory = (n*2), colorEdge = TRUE, node_label = "category")
-    cowplot::plot_grid(p1, ncol=1, labels=LETTERS[1], rel_widths=c(1))
+    P2_3.GeneNet <- cowplot::plot_grid(p1, ncol=1, labels=LETTERS[1], rel_widths=c(1))
+    P2_3.GeneNet
 
     ## 2.4 Heatmap-like functional classification
 
     ## 2.5 Enrichment Map
-    p2 <- emapplot(pairwise_termsim(y), showCategory = 10)
-    cowplot::plot_grid(p2, ncol = 1, lables = LETTERS[1])
+    p2 <- emapplot(pairwise_termsim(y), showCategory = 20)
+    p2
+    P2_5.EnrichMap <- cowplot::plot_grid(p2, ncol = 1, lables = LETTERS[1])
+    P2_5.EnrichMap
 
 
     ## 2.6 UpSet Plot
     library(ggupset) # https://github.com/const-ae/ggupset
-    upsetplot(msC2_2)
+    P2_6.UpSetPlot <- upsetplot(msC2_2)
+    P2_6.UpSetPlot
+
 
     ## 2.7 ridgeline plot for expressiong distribution
-    ridgeplot(msC2_2)
+    P2_7.RidgelinePlot <- ridgeplot(msC2_2)
+    P2_7.RidgelinePlot
+
 
     ## 2.8 gseaplot
     y2 <- arrange(msC2_2, desc(NES))
@@ -176,7 +216,9 @@
     p1 <- gseaplot(y2, geneSetID = 1, title = y2$Description[1])   # max NES
     n <- nrow(y2)
     p2 <- gseaplot(y2, geneSetID = n, title = y2$Description[n])   # min NES
-    cowplot::plot_grid(p1, p2, ncol = 1, labels = LETTERS[1:2])
+    P2_8.gseaplot <- cowplot::plot_grid(p1, p2, ncol = 1, labels = LETTERS[1:2])
+    P2_8.gseaplot
+
 
       ## 2.8.1 gseaplot2
       p3 <- gseaplot2(y2, geneSetID = 1, title = y2$Description[1])
@@ -187,24 +229,64 @@
       ## Use keyword (Overlay graphics)
       keyword <- "breast"
       ind <- grep(keyword, msC2_2$Description, ignore.case = TRUE)
-      gseaplot2(msC2_2, geneSetID = ind, title = msC2_2$Description[ind])
+      P2_8_1.gseaplot_Keyword <- gseaplot2(msC2_2, geneSetID = ind, pvalue_table=T,
+                                           base_size = 16,title = keyword,subplots = 1:3,
+                                           rel_heights = c(3, 0.5, 1))
+      P2_8_1.gseaplot_Keyword
 
       ## Overlay graphics by ID
-      gseaplot2(y2, geneSetID = 1:10)
-      gseaplot2(y2, geneSetID = 1:10, pvalue_table=T)
+      P2_8_1.gseaplot_IDTop <- gseaplot2(y2, geneSetID = 1:10, pvalue_table=T,
+                                           base_size = 16, title = "TOP 10",subplots = 1:3,
+                                           rel_heights = c(3, 0.5, 1))
+      P2_8_1.gseaplot_IDTop
 
-      ## Modify the function
-      ## https://www.biostars.org/p/9470087/
-      ## trace("gseaplot2", edit = TRUE)
-      ##> pd <- x[geneSetID, c("Description", "pvalue", "p.adjust")] -> pd <- x[geneSetID, c("Description","NES, "pvalue", "p.adjust")]
-      gseaplot2(y2, geneSetID = 1:10, pvalue_table=T)
+      P2_8_1.gseaplot_IDBot <- gseaplot2(y2, geneSetID = (length(y2@result[["ID"]])-4):length(y2@result[["ID"]]),
+                                         pvalue_table=T,
+                                        base_size = 16, title = "Bottom 10",subplots = 1:3,
+                                        rel_heights = c(3, 0.5, 1))
+      P2_8_1.gseaplot_IDBot
+
+      gseaplot2(y2, geneSetID = c(1:5,(length(y2@result[["ID"]])-4):
+                length(y2@result[["ID"]])), pvalue_table=T,
+                base_size = 16, title = "TOP 5 and Bottom 5",subplots = 1:3,
+                rel_heights = c(3, 0.5, 1)) -> P2_8_1.gseaplot_IDTopBot
+      P2_8_1.gseaplot_IDTopBot
+
+
+      # ## Modify the function
+      # ## https://www.biostars.org/p/9470087/
+      # ## trace("gseaplot2", edit = TRUE)
+      # ##> pd <- x[geneSetID, c("Description", "pvalue", "p.adjust")] -> pd <- x[geneSetID, c("Description","NES", "pvalue", "p.adjust")]
+      # gseaplot2(y2, geneSetID = 1:10, pvalue_table=T)
 
       ## 2.8.2 gsearank
-      gsearank(y2, geneSetID = 1, title = y2$Description[1])
+      P2_8_2.gsearank <- gsearank(y2, geneSetID = 1, title = y2$Description[1])
 
 
     ## 2.9 PubMed trend of enriched terms
     terms <- msC2_2$Description[1:3]
-    pmcplot(terms, 2010:2017, proportion=FALSE)
+    P2_9.PubMedTrend <- pmcplot(terms, 2010:2017, proportion=FALSE)
+    P2_9.PubMedTrend
+
+
+  #### Export PDF files ####
+    pdf(file = paste0(Save.Path,"/",ProjectName,"_clusterProfiler.pdf"),
+        width = 20,  height = 15
+    )
+      P2_1.Barplot
+      P2_2.Dotplot
+      P2_3.GeneNet
+      P2_5.EnrichMap
+      P2_6.UpSetPlot
+      P2_7.RidgelinePlot
+      P2_8.gseaplot
+      P2_8_1.gseaplot_Keyword
+      P2_8_1.gseaplot_IDTop
+      P2_8_1.gseaplot_IDBot
+      P2_8_1.gseaplot_IDTopBot
+      P2_8_2.gsearank
+      P2_9.PubMedTrend
+    dev.off()
+
 
 
