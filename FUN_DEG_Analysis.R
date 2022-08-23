1aab04b213899b6f257d4146528d8613142a2952
CharleneMSI <q2330416@gmail.com>
2022-08-23 20:35:56 +0800

    Example

--- FUN_DEG_Analysis.R
+++ FUN_DEG_Analysis.R
@@ -1,17 +1,22 @@
 #### Differential Expression Gene Analysis ####
 ## Ref: https://web.stanford.edu/class/bios221/labs/rnaseq/lab_4_rnaseq.html
+## Ref: https://www.jianshu.com/p/b6912d318de5
 
 FUN_DEG_Analysis = function(GeneExp.df,
                             TarGeneName = TarGene_name, GroupMode = Mode_Group,
                             Save.Path = Save.Path, SampleName = SampleName
 ){
+  ##### Parameter setting* #####
+  # Set the desired organism
+  organism = "org.Dm.eg.db"
 
 
   #### BiocManager installation ####
   ## Check whether the installation of those packages is required from BiocManager
   if (!require("BiocManager", quietly = TRUE))
     install.packages("BiocManager")
-  Package.set <- c(organism,"edgeR")
+  # BiocManager::install()
+  Package.set <- c(organism,"edgeR","baySeq")
   for (i in 1:length(Package.set)) {
     if (!requireNamespace(Package.set[i], quietly = TRUE)){
       BiocManager::install(Package.set[i])
@@ -22,6 +27,85 @@ FUN_DEG_Analysis = function(GeneExp.df,
   rm(Package.set,i)
 
   #### Differential Expression Gene Analysis ####
+
+  #### Example ####
+  library(baySeq)
+  load("mobData.RData")
+
+  con <- url("https://bios221.stanford.edu/data/mobData.RData") # Create connexion
+  load(con) #Load the data
+
+  head(mobData)
+
+  #help(mobData)
+  mobDataGroups <- c("MM", "MM", "WM", "WM", "WW", "WW")
+  # MM="triple mutatnt shoot grafted onto triple mutant root"
+  # WM="wild-type shoot grafted onto triple mutant root"
+  # WW="wild-type shoot grafted onto wild-type root"
+  data(mobAnnotation)
+  #?mobAnnotation
+  head(mobAnnotation)
+
+  d <- DGEList(counts=mobData,group=factor(mobDataGroups))
+  d
+
+  ## Filtering the data
+  dim(d)
+
+  d.full <- d # keep the old one in case we mess up
+  head(d$counts)
+  head(cpm(d))
+
+  apply(d$counts, 2, sum) # total gene counts per sample
+
+  keep <- rowSums(cpm(d)>100) >= 2
+  d <- d[keep,]
+  dim(d)
+
+  d$samples$lib.size <- colSums(d$counts)
+  d$samples
+
+  ## Normalizing the data
+  d <- calcNormFactors(d)
+  d
+
+  ## Data Exploration
+  plotMDS(d, method="bcv", col=as.numeric(d$samples$group))
+  legend("bottomleft", as.character(unique(d$samples$group)), col=1:3, pch=20)
+
+  ## Estimating the Dispersion
+  d1 <- estimateCommonDisp(d, verbose=T)
+  names(d1)
+
+  d1 <- estimateTagwiseDisp(d1)
+  names(d1)
+
+  plotBCV(d1)
+
+  ## GLM estimates of dispersion
+  design.mat <- model.matrix(~ 0 + d$samples$group)
+  colnames(design.mat) <- levels(d$samples$group)
+  d2 <- estimateGLMCommonDisp(d,design.mat)
+  d2 <- estimateGLMTrendedDisp(d2,design.mat, method="power")
+  # You can change method to "auto", "bin.spline", "power", "spline", "bin.loess".
+  # The default is "auto" which chooses "bin.spline" when > 200 tags and "power" otherwise.
+  d2 <- estimateGLMTagwiseDisp(d2,design.mat)
+  plotBCV(d2)
+
+  ## Comparing the models in DESeq and edgeR
+
+  require(DESeq2)
+  cds <- newCountDataSet( data.frame(d$counts), d$samples$group )
+  cds <- estimateSizeFactors( cds )
+  sizeFactors( cds )
+
+  cds <- estimateDispersions( cds , method="blind")
+  plotDispEsts(cds)
+
+
+
+
+  ###############################################
   library(edgeR)
   Anno_Ints.df <- Anno.df[Anno.df$ReCluster %in% c("AD","AC"),]  # c("CoreCD00","CDOri")
   matrix_Ints.df <- matrix.df[,colnames(matrix.df) %in% Anno_Ints.df$CELL]
