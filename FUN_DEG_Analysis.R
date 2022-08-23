1c26cf77bb79fc542ec5533de56444ddfcae09ec
Charlene CAESAR Lab <q2330416@gmail.com>
2022-08-23 18:41:21 +0800

    BiocManager installation for edgeR

--- FUN_DEG_Analysis.R
+++ FUN_DEG_Analysis.R
@@ -6,6 +6,22 @@ FUN_DEG_Analysis = function(GeneExp.df,
                             Save.Path = Save.Path, SampleName = SampleName
 ){
 
+
+  #### BiocManager installation ####
+  ## Check whether the installation of those packages is required from BiocManager
+  if (!require("BiocManager", quietly = TRUE))
+    install.packages("BiocManager")
+  Package.set <- c(organism,"edgeR")
+  for (i in 1:length(Package.set)) {
+    if (!requireNamespace(Package.set[i], quietly = TRUE)){
+      BiocManager::install(Package.set[i])
+    }
+  }
+  ## Load Packages
+  lapply(Package.set, library, character.only = TRUE)
+  rm(Package.set,i)
+
+  #### Differential Expression Gene Analysis ####
   library(edgeR)
   Anno_Ints.df <- Anno.df[Anno.df$ReCluster %in% c("AD","AC"),]  # c("CoreCD00","CDOri")
   matrix_Ints.df <- matrix.df[,colnames(matrix.df) %in% Anno_Ints.df$CELL]
@@ -19,6 +35,11 @@ FUN_DEG_Analysis = function(GeneExp.df,
   DE_Extract.df <- topTags(DE_Extract.lt,n = nrow(matrix_Ints.df)) %>%
     as.data.frame() %>%
     data.frame(Gene=row.names(.),.)
+
+  #### Export file ####
+
+
+  #### Output ####
 
 
   return(Output)
