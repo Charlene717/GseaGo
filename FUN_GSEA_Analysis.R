1edd33d10c42287ccc273a43f296a49f80ef0efa
Charlene CAESAR Lab <q2330416@gmail.com>
2022-08-24 15:49:56 +0800

    Comment out one GSEA method

--- FUN_GSEA_Analysis.R
+++ FUN_GSEA_Analysis.R
@@ -15,13 +15,10 @@
 
 FUN_GSEA_Analysis = function(DE_Extract.df, pathwayGeneSet = Pathway.all,
                              TarGeneName = TarGene_name, GroupMode = Mode_Group,
-                             ThrSet = Thr.lt, Species = "Homo sapiens", # Speices type can check by msigdbr_species()
+                             Species = "Homo sapiens", # Speices type can check by msigdbr_species()
                              Save.Path = Save.Path, SampleName = SampleName, AnnoName = "C2"
 ){
 
-  ##### Parameter setting* #####
-    ## Set the desired organism
-    organism = "org.Hs.eg.db" ## c("org.Hs.eg.db","org.Mm.eg.db")   ##  c("org.Dm.eg.db")
 
   ##### Load Packages  #####
     #### Basic installation ####
@@ -54,111 +51,105 @@ FUN_GSEA_Analysis = function(DE_Extract.df, pathwayGeneSet = Pathway.all,
     options(stringsAsFactors = FALSE)
 
 
-  ##### Load Files #####
-    # ## Load data
-    # load("D:/Dropbox/##_GitHub/##_CAESAR/GseaGo/Demo_data/Robjects/Annotated_Results_LvV.RData")
-    #
-    # ## Load pathways
-    # load("D:/Dropbox/##_GitHub/##_CAESAR/GseaGo/Demo_data/Robjects/mouse_H_v5.RData")
-    # pathwayGeneSet <- Mm.H
-
-  ##### GSEA analysis (fgsea) #####
-
-
-    #### Create ranks ####
-      # gseaDat2 <- filter(shrinkLvV, !is.na(shrinkLvV[,"Entrez"]))
-      # gseaDat <- filter(shrinkLvV, !is.na(Entrez))
-      # ranks <- gseaDat$logFC
-      # names(ranks) <- gseaDat$Entrez
-      # head(ranks)
-      #
-      # # Plot the ranked fold changes.
-      # barplot(sort(ranks, decreasing = T))
-
-      ranks <- DE_Extract.df[,ThrSet[["LogFC"]][1]]
-      names(ranks) <- row.names(DE_Extract.df)
-      head(ranks)
-
-      # Plot the ranked fold changes.
-      barplot(sort(ranks, decreasing = T))
-
-    #### Transform pathwayGeneSet to list ####
-      # pathwayGeneSet.lt <- as.list(pathwayGeneSet[,c(-1,-2)])
-
-      # pathwayGeneSet.lt <- split(pathwayGeneSet[,c(-1,-2)], 1:nrow(pathwayGeneSet))
-      # names(pathwayGeneSet.lt) <- pathwayGeneSet[,1]
-
-      ## No use
-      # pathwayGeneSet.lt2 <- pathwayGeneSet.lt[!is.na(pathwayGeneSet.lt)]
-      # pathwayGeneSet.lt2 <- lapply(pathwayGeneSet.lt, function(x) unique(x))
-
-      ## EXist ""
-      # pathwayGeneSet.lt <- apply(pathwayGeneSet[,c(-1,-2)], 1, function(a)unique(a))
-      # names(pathwayGeneSet.lt) <- pathwayGeneSet[,1]
-
-      pathwayGeneSet.lt <- apply(pathwayGeneSet[,c(-1,-2)], 1, function(a)unique(a[a != ""]))
-      names(pathwayGeneSet.lt) <- pathwayGeneSet[,1]
-
-    #### Conduct analysis ####
-      fgseaRes <- fgsea(pathwayGeneSet.lt, ranks, minSize=15, maxSize = 500, nperm=1000)
-
-      ## fgsea: What does fgseaMultilevel argument sampleSize mean/when to change it?
-      ## https://www.biostars.org/p/479821/
-
-      fgseaRes <- fgseaMultilevel(pathwayGeneSet.lt, ranks, minSize=15, maxSize = 500)
-      ## Error when running parallelized process: Warning in serialize... package:stats may not be available when loading
-      ## https://community.rstudio.com/t/error-when-running-parallelized-process-warning-in-serialize-package-stats-may-not-be-available-when-loading/110573
-      ## https://stackoverflow.com/questions/27623901/r-warning-packagestats-may-not-be-available-when-loading
-
-      head(fgseaRes[order(padj, -abs(NES)), ], n=10)
-
-    #### Enrichment score plot ####
-      # plotEnrichment(pathwayGeneSet[["HALLMARK_ESTROGEN_RESPONSE_EARLY"]], ranks)
-
-      plotEnrichment(pathwayGeneSet[pathwayGeneSet[,1]=="HALLMARK_ESTROGEN_RESPONSE_EARLY",], ranks)
-      dev.off()
-
-    #### GSEA table plot ####
-      # topUp <- fgseaRes %>%
-      #   filter(ES > 0) %>%
-      #   top_n(10, wt=-padj)
-      # topDown <- fgseaRes %>%
-      #   filter(ES < 0) %>%
-      #   top_n(10, wt=-padj)
-      # topPathways <- bind_rows(topUp, topDown) %>%
-      #   arrange(-ES)
-      # plotGseaTable(pathwayGeneSet[topPathways$pathway],
-      #               ranks,
-      #               fgseaRes,
-      #               gseaParam = 0.5)
-      # dev.off()
-
-      topUp <- fgseaRes %>%
-               filter(NES > 0) %>%
-               top_n(10, wt=NES)
-      topDown <- fgseaRes %>%
-                 filter(NES < 0) %>%
-                 top_n(10, wt=-NES)
-      topPathways <- bind_rows(topUp, topDown) %>%
-                     arrange(-NES)
-      plotGseaTable(pathwayGeneSet.lt[topPathways$pathway],
-                    ranks,
-                    fgseaRes,
-                    gseaParam = 0.5)
-      dev.off()
-
-      ## 2.1 Barplot
-      library(ggstance)
-      ggplot(topPathways, aes(NES, fct_reorder(pathway, NES), fill = pval), showCategory=(n*2)) +
-             geom_barh(stat='identity') +
-             scale_fill_continuous(low='red', high='blue', guide=guide_colorbar(reverse=TRUE)) +
-             theme_minimal() + ylab(NULL)
+# #************************************************************************************************************************#
+#     ##### Parameter setting* #####
+#     ## Set the desired organism
+#     organism = "org.Hs.eg.db" ## c("org.Hs.eg.db","org.Mm.eg.db")   ##  c("org.Dm.eg.db")
+#
+#   ##### GSEA analysis (fgsea) #####
+#
+#
+#     #### Create ranks ####
+#       # gseaDat2 <- filter(shrinkLvV, !is.na(shrinkLvV[,"Entrez"]))
+#       # gseaDat <- filter(shrinkLvV, !is.na(Entrez))
+#       # ranks <- gseaDat$logFC
+#       # names(ranks) <- gseaDat$Entrez
+#       # head(ranks)
+#       #
+#       # # Plot the ranked fold changes.
+#       # barplot(sort(ranks, decreasing = T))
+#
+#       ranks <- DE_Extract.df[,ThrSet[["LogFC"]][1]]
+#       names(ranks) <- row.names(DE_Extract.df)
+#       head(ranks)
+#
+#       # Plot the ranked fold changes.
+#       barplot(sort(ranks, decreasing = T))
+#
+#     #### Transform pathwayGeneSet to list ####
+#       # pathwayGeneSet.lt <- as.list(pathwayGeneSet[,c(-1,-2)])
+#
+#       # pathwayGeneSet.lt <- split(pathwayGeneSet[,c(-1,-2)], 1:nrow(pathwayGeneSet))
+#       # names(pathwayGeneSet.lt) <- pathwayGeneSet[,1]
+#
+#       ## No use
+#       # pathwayGeneSet.lt2 <- pathwayGeneSet.lt[!is.na(pathwayGeneSet.lt)]
+#       # pathwayGeneSet.lt2 <- lapply(pathwayGeneSet.lt, function(x) unique(x))
+#
+#       ## EXist ""
+#       # pathwayGeneSet.lt <- apply(pathwayGeneSet[,c(-1,-2)], 1, function(a)unique(a))
+#       # names(pathwayGeneSet.lt) <- pathwayGeneSet[,1]
+#
+#       pathwayGeneSet.lt <- apply(pathwayGeneSet[,c(-1,-2)], 1, function(a)unique(a[a != ""]))
+#       names(pathwayGeneSet.lt) <- pathwayGeneSet[,1]
+#
+#     #### Conduct analysis ####
+#       fgseaRes <- fgsea(pathwayGeneSet.lt, ranks, minSize=15, maxSize = 500, nperm=1000)
+#
+#       ## fgsea: What does fgseaMultilevel argument sampleSize mean/when to change it?
+#       ## https://www.biostars.org/p/479821/
+#
+#       fgseaRes <- fgseaMultilevel(pathwayGeneSet.lt, ranks, minSize=15, maxSize = 500)
+#       ## Error when running parallelized process: Warning in serialize... package:stats may not be available when loading
+#       ## https://community.rstudio.com/t/error-when-running-parallelized-process-warning-in-serialize-package-stats-may-not-be-available-when-loading/110573
+#       ## https://stackoverflow.com/questions/27623901/r-warning-packagestats-may-not-be-available-when-loading
+#
+#       head(fgseaRes[order(padj, -abs(NES)), ], n=10)
+#
+#     #### Enrichment score plot ####
+#       # plotEnrichment(pathwayGeneSet[["HALLMARK_ESTROGEN_RESPONSE_EARLY"]], ranks)
+#
+#       plotEnrichment(pathwayGeneSet[pathwayGeneSet[,1]=="HALLMARK_ESTROGEN_RESPONSE_EARLY",], ranks)
+#       dev.off()
+#
+#     #### GSEA table plot ####
+#       # topUp <- fgseaRes %>%
+#       #   filter(ES > 0) %>%
+#       #   top_n(10, wt=-padj)
+#       # topDown <- fgseaRes %>%
+#       #   filter(ES < 0) %>%
+#       #   top_n(10, wt=-padj)
+#       # topPathways <- bind_rows(topUp, topDown) %>%
+#       #   arrange(-ES)
+#       # plotGseaTable(pathwayGeneSet[topPathways$pathway],
+#       #               ranks,
+#       #               fgseaRes,
+#       #               gseaParam = 0.5)
+#       # dev.off()
+#
+#       topUp <- fgseaRes %>%
+#                filter(NES > 0) %>%
+#                top_n(10, wt=NES)
+#       topDown <- fgseaRes %>%
+#                  filter(NES < 0) %>%
+#                  top_n(10, wt=-NES)
+#       topPathways <- bind_rows(topUp, topDown) %>%
+#                      arrange(-NES)
+#       plotGseaTable(pathwayGeneSet.lt[topPathways$pathway],
+#                     ranks,
+#                     fgseaRes,
+#                     gseaParam = 0.5)
+#       dev.off()
+#
+#       ## 2.1 Barplot
+#       library(ggstance)
+#       ggplot(topPathways, aes(NES, fct_reorder(pathway, NES), fill = pval), showCategory=(n*2)) +
+#              geom_barh(stat='identity') +
+#              scale_fill_continuous(low='red', high='blue', guide=guide_colorbar(reverse=TRUE)) +
+#              theme_minimal() + ylab(NULL)
 
 
-
-
-
-
+#************************************************************************************************************************#
   ##### GSEA analysis #####
       #### Conduct analysis2 ####
       library(DOSE)
@@ -190,13 +181,13 @@ FUN_GSEA_Analysis = function(DE_Extract.df, pathwayGeneSet = Pathway.all,
       rm(Temp)
 
       #### RUN GSEA ####
-      msC2_2 <- GSEA(geneList, TERM2GENE = m_c2) #       msC2_2 <- GSEA(geneList, TERM2GENE = m_c2)
+      GSEA_Result <- GSEA(geneList, TERM2GENE = m_c2) #       GSEA_Result <- GSEA(geneList, TERM2GENE = m_c2)
 
       #### Visualization ####
 
       ## 2.1 Barplot
       library(clusterProfiler.dplyr) # devtools::install_github("YuLab-SMU/clusterProfiler.dplyr")
-      y <- mutate(msC2_2, ordering = abs(NES)) %>%
+      y <- mutate(GSEA_Result, ordering = abs(NES)) %>%
            arrange(desc(ordering))
 
       library(ggstance)
@@ -213,13 +204,13 @@ FUN_GSEA_Analysis = function(DE_Extract.df, pathwayGeneSet = Pathway.all,
             theme_minimal() + ylab(NULL)
 
       ## 2.2 Dotplot
-      dotplot(msC2_2, showCategory = 10, font.size = 8,
+      dotplot(GSEA_Result, showCategory = 10, font.size = 8,
               x = "GeneRatio",   # option -> c("GeneRatio", "Count")
               color = "p.adjust")   # option -> c("pvalue", "p.adjust", "qvalue")
 
       ## 2.3 Gene-Concept Network
       n <- 3
-      p1 <- cnetplot(msC2_2, showCategory = (n*2), colorEdge = TRUE, node_label = "category")
+      p1 <- cnetplot(GSEA_Result, showCategory = (n*2), colorEdge = TRUE, node_label = "category")
       cowplot::plot_grid(p1, ncol=1, labels=LETTERS[1], rel_widths=c(1))
 
       ## 2.4 Heatmap-like functional classification
@@ -233,13 +224,13 @@ FUN_GSEA_Analysis = function(DE_Extract.df, pathwayGeneSet = Pathway.all,
 
       ## 2.6 UpSet Plot
       library(ggupset)
-      upsetplot(msC2_2)
+      upsetplot(GSEA_Result)
 
       ## 2.7 ridgeline plot for expressiong distribution
-      ridgeplot(msC2_2)
+      ridgeplot(GSEA_Result)
 
       ## 2.8 gseaplot
-      y2 <- arrange(msC2_2, desc(NES))
+      y2 <- arrange(GSEA_Result, desc(NES))
 
       p1 <- gseaplot(y2, geneSetID = 1, title = y2$Description[1])   # max NES
       n <- nrow(y2)
@@ -252,9 +243,11 @@ FUN_GSEA_Analysis = function(DE_Extract.df, pathwayGeneSet = Pathway.all,
       cowplot::plot_grid(p3, p4, ncol = 1, labels = LETTERS[1:2])
 
       ## Use keyword (Overlay graphics)
-      keyword <- "breast"
-      ind <- grep(keyword, msC2_2$Description, ignore.case = TRUE)
-      gseaplot2(msC2_2, geneSetID = ind, title = msC2_2$Description[ind])
+      try({
+        keyword <- "breast"
+        ind <- grep(keyword, GSEA_Result$Description, ignore.case = TRUE)
+        gseaplot2(GSEA_Result, geneSetID = ind, title = GSEA_Result$Description[ind])
+      })
 
       ## Overlay graphics by ID
       gseaplot2(y2, geneSetID = 1:10)
@@ -264,9 +257,28 @@ FUN_GSEA_Analysis = function(DE_Extract.df, pathwayGeneSet = Pathway.all,
 
 
       ## 2.9 PubMed trend of enriched terms
-      terms <- msC2_2$Description[1:3]
+      terms <- GSEA_Result$Description[1:3]
       pmcplot(terms, 2010:2017, proportion=FALSE)
-      dev.off()
+      # dev.off()
+
+    # ##### Export Result #####
+    #   pdf(
+    #     file = paste0(Save.Path,"/",SampleName,"_",TarGeneName,"_DensityPlot.pdf"),
+    #     width = 10,  height = 8
+    #   )
+    #   print(TGeneDen_SD.p)
+    #   print(TGeneDen_Q.p)
+    #   print(TGeneDen_SD_Q.p)
+    #
+    #   dev.off()
+
+    #### Output ####
+      Output <- list()
+      Output[["GSEA_Result"]] <- GSEA_Result
+      Output[["geneList_ranks"]] <- geneList
+
+    return(Output)
+
 
 return(Output)
 
