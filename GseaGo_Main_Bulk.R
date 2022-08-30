07ef0100bb4c560f767c6cde3f0cdf634eb29260
CharleneMSI <q2330416@gmail.com>
2022-08-30 13:59:15 +0800

    Modify the writing way of loading function.

--- GseaGo_Main_Bulk.R
+++ GseaGo_Main_Bulk.R
@@ -3,19 +3,45 @@
   memory.limit(150000)
 
 ##### Load Packages #####
-  if(!require("tidyverse")) install.packages("tidyverse")
-  if(!require("Seurat")) install.packages("Seurat")
-  if(!require("SeuratData")) install.packages("SeuratData")
-  if(!require("patchwork")) install.packages("patchwork")
-  if(!require("plyr")) install.packages("plyr")
-  if(!require("eoffice")) install.packages("eoffice")
+  # if(!require("tidyverse")) install.packages("tidyverse")
+  # library(tidyverse)
 
-  library(tidyverse)
-  library(Seurat)
-  library(SeuratData)
-  library(patchwork)
-  library(plyr)
-  library(eoffice)
+  #### Basic installation ####
+  ## Check whether the installation of those packages is required from basic
+  Package.set <- c("tidyverse","ggplot2","Seurat","SeuratData","patchwork","plyr","eoffice","DT")
+  for (i in 1:length(Package.set)) {
+    if (!requireNamespace(Package.set[i], quietly = TRUE)){
+      install.packages(Package.set[i])
+    }
+  }
+  ## Load Packages
+  lapply(Package.set, library, character.only = TRUE)
+  rm(Package.set,i)
+
+
+  #### BiocManager installation ####
+  ## Set the desired organism
+  # organism = "org.Hs.eg.db" ## c("org.Hs.eg.db","org.Mm.eg.db")   ##  c("org.Dm.eg.db")
+
+  ## Check whether the installation of those packages is required from BiocManager
+  if (!require("BiocManager", quietly = TRUE))
+    install.packages("BiocManager")
+  Package.set <- c("clusterProfiler","enrichplot","pathview") # c(organism,"fgsea","clusterProfiler","enrichplot","pathview")
+  for (i in 1:length(Package.set)) {
+    if (!requireNamespace(Package.set[i], quietly = TRUE)){
+      BiocManager::install(Package.set[i])
+    }
+  }
+  ## Load Packages
+  lapply(Package.set, library, character.only = TRUE)
+  rm(Package.set,i)
+
+  # options(stringsAsFactors = FALSE)
+
+
+  # Sys.setlocale(category = "LC_ALL", locale = "UTF-8")
+
+
 
 ##### Function setting #####
   ## Call function
