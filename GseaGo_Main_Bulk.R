bd6c9227b12cb8c40eb91c96f4135f6470ab6382
Charlene CAESAR Lab <q2330416@gmail.com>
2022-09-03 22:24:09 +0800

    Use FUN_Package_InstLoad

--- GseaGo_Main_Bulk.R
+++ GseaGo_Main_Bulk.R
@@ -6,42 +6,15 @@
   # if(!require("tidyverse")) install.packages("tidyverse")
   # library(tidyverse)
 
-  #### Basic installation ####
-  ## Check whether the installation of those packages is required from basic
-  Package.set <- c("tidyverse","ggplot2","Seurat","SeuratData","patchwork","plyr","eoffice","DT")
-  for (i in 1:length(Package.set)) {
-    if (!requireNamespace(Package.set[i], quietly = TRUE)){
-      install.packages(Package.set[i])
-    }
-  }
-  ## Load Packages
-  lapply(Package.set, library, character.only = TRUE)
-  rm(Package.set,i)
+  source("FUN_Package_InstLoad.R")
+  Basic.set <- c("tidyverse","ggplot2","Seurat","SeuratData","patchwork","plyr","eoffice","DT")
 
-
-  #### BiocManager installation ####
+  BiocManager.set <- c("clusterProfiler","enrichplot","pathview")
   ## Set the desired organism
-  # organism = "org.Hs.eg.db" ## c("org.Hs.eg.db","org.Mm.eg.db")   ##  c("org.Dm.eg.db")
+  # organism = "org.Hs.eg.db" ## c("org.Hs.eg.db","org.Mm.eg.db","org.Dm.eg.db")
+  # c(organism,"fgsea")
 
-  ## Check whether the installation of those packages is required from BiocManager
-  if (!require("BiocManager", quietly = TRUE))
-    install.packages("BiocManager")
-  Package.set <- c("clusterProfiler","enrichplot","pathview") # c(organism,"fgsea","clusterProfiler","enrichplot","pathview")
-  for (i in 1:length(Package.set)) {
-    if (!requireNamespace(Package.set[i], quietly = TRUE)){
-      BiocManager::install(Package.set[i])
-    }
-  }
-  ## Load Packages
-  lapply(Package.set, library, character.only = TRUE)
-  rm(Package.set,i)
-
-  # options(stringsAsFactors = FALSE)
-
-
-  # Sys.setlocale(category = "LC_ALL", locale = "UTF-8")
-
-
+  Package_InstLoad(Basic.set = Basic.set, BiocManager.set = BiocManager.set)
 
 ##### Function setting #####
   ## Call function
