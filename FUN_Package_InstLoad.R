636d20fbe7163ce28f8554ca8e3a67aaf9ebca84
Charlene CAESAR Lab <q2330416@gmail.com>
2022-09-03 22:23:39 +0800

    Clean up the code

--- FUN_Package_InstLoad.R
+++ FUN_Package_InstLoad.R
@@ -1,5 +1,5 @@
 Package_InstLoad = function( Basic.set = Basic.set,
-                                 BiocManager.set = BiocManager.set)
+                             BiocManager.set = BiocManager.set)
 {
 
   ##### Load Packages #####
@@ -8,37 +8,31 @@ Package_InstLoad = function( Basic.set = Basic.set,
 
   #### Basic installation ####
   ## Check whether the installation of those packages is required from basic
-  Package.set <- Basic.set
-  for (i in 1:length(Package.set)) {
-    if (!requireNamespace(Package.set[i], quietly = TRUE)){
-      install.packages(Package.set[i])
+  for (i in 1:length(Basic.set)) {
+    if (!requireNamespace(Basic.set[i], quietly = TRUE)){
+      install.packages(Basic.set[i])
     }
   }
   ## Load Packages
-  lapply(Package.set, library, character.only = TRUE)
-  rm(Package.set,i)
+  lapply(Basic.set, library, character.only = TRUE)
+  rm(Basic.set,i)
 
 
   #### BiocManager installation ####
-  ## Set the desired organism
-  # organism = "org.Hs.eg.db" ## c("org.Hs.eg.db","org.Mm.eg.db")   ##  c("org.Dm.eg.db")
-
   ## Check whether the installation of those packages is required from BiocManager
   if (!require("BiocManager", quietly = TRUE))
     install.packages("BiocManager")
-  Package.set <- BiocManager.set
-  for (i in 1:length(Package.set)) {
-    if (!requireNamespace(Package.set[i], quietly = TRUE)){
-      BiocManager::install(Package.set[i])
+  BiocManager.set <- BiocManager.set
+  for (i in 1:length(BiocManager.set)) {
+    if (!requireNamespace(BiocManager.set[i], quietly = TRUE)){
+      BiocManager::install(BiocManager.set[i])
     }
   }
   ## Load Packages
-  lapply(Package.set, library, character.only = TRUE)
-  rm(Package.set,i)
+  lapply(BiocManager.set, library, character.only = TRUE)
+  rm(BiocManager.set,i)
 
   # options(stringsAsFactors = FALSE)
-
-
   # Sys.setlocale(category = "LC_ALL", locale = "UTF-8")
 
 }
