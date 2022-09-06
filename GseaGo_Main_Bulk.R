48657a1c5df4a12b673f3f5727a8e8f6d8640f21
CharleneMSI <q2330416@gmail.com>
2022-09-06 18:14:33 +0800

    Change the way of Update genename

--- GseaGo_Main_Bulk.R
+++ GseaGo_Main_Bulk.R
@@ -6,7 +6,7 @@
   source("FUN_Package_InstLoad.R")
   Basic.set <- c("tidyverse","ggplot2","Seurat","SeuratData","patchwork","plyr","eoffice","DT")
 
-  BiocManager.set <- c("clusterProfiler","enrichplot","pathview")
+  BiocManager.set <- c("clusterProfiler","enrichplot","pathview","limma")
   ## Set the desired organism
   # organism = "org.Hs.eg.db" ## c("org.Hs.eg.db","org.Mm.eg.db","org.Dm.eg.db")
   # c(organism,"fgsea")
@@ -99,71 +99,11 @@
 
 
 ##### Update the genename ####
-
   ## Ref: http://web.mit.edu/~r/current/arch/i386_linux26/lib/R/library/limma/html/alias2Symbol.html
-  library(limma)
-
-  UpdateGene <- function(TestGeneName, Species = "Hs") {
-    UpdateGeneName <- alias2Symbol(TestGeneName, species = Species, expand.symbols = FALSE)
-    if( length(UpdateGeneName) == 0 ){
-      TestGeneName <- TestGeneName
-    }else{
-      TestGeneName <- UpdateGeneName
-    }
-
-    if(length(TestGeneName) > 1){
-      TestGeneName <- TestGeneName[1]
-    }
-    return(TestGeneName)
-  }
-
-  # ## Test UpdateGene function
-  # TestGene <- "SEPT1"
-  # TestGene <- "HBII-52-46"
-  # TestGene <- UpdateGene(TestGene)
-
-  TTT <- GeneExp.df
-  # EX1 <- lapply(1:3, function(x)x^x)
-  # TTT2 <- lapply(row.names(TTT), function(x)UpdateGene(x))
-  # # TTT2 <- lapply(1:3, function(x)UpdateGene(x))
-  # TTT3 <- unlist(TTT2) %>% as.data.frame()
-  TTT3 <- lapply(row.names(TTT), function(x)UpdateGene(x))  %>% unlist() %>% as.data.frame()
-
-  df <- cbind(row.names(TTT),TTT3[,1]) %>% as.data.frame()
-  sum(df[,1] != df[,2])
-
-  ## Find Duplicate name
-  ## Ref: http://guangzheng.name/2017/10/07/%E5%A6%82%E4%BD%95%E6%9F%A5%E6%89%BE%E6%95%B0%E6%8D%AE%E6%A1%86%E4%B8%AD%E9%87%8D%E5%A4%8D%E7%9A%84%E6%95%B0%E6%8D%AE/
-  library(dplyr)
-  df %>% group_by(V2) %>%
-         mutate(index = n()) %>%
-         filter(index > 1) %>%
-         select(2) %>%
-         ungroup() %>%
-         unique() %>%
-         unlist() -> Dup.set
-
-  # x=11
-  UpdateGeneDUPE <- function(df,x) {
-    if( (df[x,2] %in% Dup.set)== TRUE ){
-      df[x,1] = df[x,1]
-    }else{
-      df[x,1] = df[x,2]
-    }
-    return(df[x,1])
-  }
-
-  df2 <- lapply(1:nrow(df), function(x)UpdateGeneDUPE(df,x))  %>% as.data.frame()
-  row.names(TTT) <- df2
-
-
-
-  ## Update the genename ##
   library(limma)
   UpdateGene <- "No"  # UpdateGene <- c("Yes","No")
   if(UpdateGene == "Yes"){
-    ## Error ##  row.names(GeneExp.df) <- alias2Symbol(row.names(GeneExp.df), species = "Hs", expand.symbols = FALSE)
-    row.names(GeneExp.df) <- alias2Symbol(row.names(GeneExp.df), species = "Hs", expand.symbols = FALSE)
+    source("RUN_FUN_UpdateGeneName.R")
 
   }
 
