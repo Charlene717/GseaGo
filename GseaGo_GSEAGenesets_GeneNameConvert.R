0c38ecd79a1a26bd64ab2aade12e445106b384ba
CharleneMSI <q2330416@gmail.com>
2022-09-07 17:20:37 +0800

    temporary backup

--- GseaGo_GSEAGenesets_GeneNameConvert.R
+++ GseaGo_GSEAGenesets_GeneNameConvert.R
@@ -1,3 +1,38 @@
+##### 09_0 GSEA Analysis (Geneset Prepare) #####
+# https://bioinformatics-core-shared-training.github.io/cruk-summer-school-2018/RNASeq2018/html/06_Gene_set_testing.nb.html
+# install # https://bioconductor.org/packages/release/bioc/html/GSEABase.html
+library(fgsea)
+source("FUN_GSEA_LargeGeneSet.R")
+source("FUN_HSsymbol2MMsymbol.R")
+source("FUN_GSEA_ggplot.R")
+
+# Geneset from GSEA
+# Pathway.all <- read.delim(paste0(getwd(),"/Pathway.all.v7.4.symbols.gmt"),header = F)
+Pathway.all <- read.delim2(paste0(getwd(),"/GSEA_Geneset/GSEA_Geneset_Pathway_3Database_WithoutFilter.txt"),
+                           col.names = 1:max(count.fields(paste0(getwd(),"/GSEA_Geneset/GSEA_Geneset_Pathway_3Database_WithoutFilter.txt"))),
+                           header = F,sep = "\t")
+
+# Convert Human gene to mouse
+Pathway.all.MM = as.data.frame(matrix(nrow=nrow(Pathway.all),ncol=ncol(Pathway.all)*1.5))
+for (i in 1:nrow(Pathway.all)) {
+  #Pathway.all[,i] <- data.frame(colnames(Pathway.all)[i]=Pathway.all[,i]) %>% HSsymbol2MMsymbol(.,colnames(Pathway.all)[i])
+  PathwayN <- data.frame(Pathway.all[i,3:ncol(Pathway.all)]) %>% t()
+  colnames(PathwayN)="Test"
+  PathwayN <- HSsymbol2MMsymbol(PathwayN,"Test")
+  Pathway.all.MM[i,1:length(unique(PathwayN$MM.symbol))] <- unique(PathwayN$MM.symbol)
+
+}
+
+Pathway.all.MM <- data.frame(Pathway.all[,1:2],Pathway.all.MM)
+colnames(Pathway.all.MM) <- seq(1:ncol(Pathway.all.MM))
+Pathway.all.MM[is.na(Pathway.all.MM)] <- ""
+
+#### Save RData ####
+save.image(paste0(Save.Path,"/09_0_GSEA_Analysis(Geneset_Prepare).RData"))
+
+
+
+
 
 
 #
