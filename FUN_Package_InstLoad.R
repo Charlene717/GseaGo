d4072dc338c09c76e00d5a1d593e6e7f33c9f7d0
Charlene <q2330416@gmail.com>
2023-02-04 11:49:29 +0800

    Check if the package exists

--- FUN_Package_InstLoad.R
+++ FUN_Package_InstLoad.R
@@ -1,38 +1,50 @@
-Package_InstLoad = function( Basic.set = Basic.set,
-                             BiocManager.set = BiocManager.set)
-{
+####-------------------- Load Packages --------------------###
 
-  ##### Load Packages #####
-  # if(!require("tidyverse")) install.packages("tidyverse")
-  # library(tidyverse)
+#### For few packages ####
+# if(!require("tidyverse")) install.packages("tidyverse")
+# library(tidyverse)
+
+#### Set the desired organism ####
+# organism = "org.Hs.eg.db" ## c("org.Hs.eg.db","org.Mm.eg.db","org.Dm.eg.db")
+# c(organism,"fgsea")
+
+#### For Many packages  ####
+FUN_Package_InstLoad = function( Basic.set = Basic.set,
+                                 BiocManager.set = BiocManager.set)
+{
 
   #### Basic installation ####
   ## Check whether the installation of those packages is required from basic
-  for (i in 1:length(Basic.set)) {
-    if (!requireNamespace(Basic.set[i], quietly = TRUE)){
-      install.packages(Basic.set[i])
+  if(exists("Basic.set")==TRUE){
+
+    for (i in 1:length(Basic.set)) {
+      if (!requireNamespace(Basic.set[i], quietly = TRUE)){
+        install.packages(Basic.set[i])
+      }
     }
+    ## Load Packages
+    lapply(Basic.set, library, character.only = TRUE)
+    rm(Basic.set,i)
+
   }
-  ## Load Packages
-  lapply(Basic.set, library, character.only = TRUE)
-  rm(Basic.set,i)
 
 
   #### BiocManager installation ####
   ## Check whether the installation of those packages is required from BiocManager
-  if (!require("BiocManager", quietly = TRUE))
-    install.packages("BiocManager")
-  BiocManager.set <- BiocManager.set
-  for (i in 1:length(BiocManager.set)) {
-    if (!requireNamespace(BiocManager.set[i], quietly = TRUE)){
-      BiocManager::install(BiocManager.set[i])
-    }
-  }
-  ## Load Packages
-  lapply(BiocManager.set, library, character.only = TRUE)
-  rm(BiocManager.set,i)
+  if(exists("FUN_BiocManager.set")==TRUE){
 
-  # options(stringsAsFactors = FALSE)
-  # Sys.setlocale(category = "LC_ALL", locale = "UTF-8")
+    if (!require("BiocManager", quietly = TRUE))
+      install.packages("BiocManager")
+    BiocManager.set <- BiocManager.set
+    for (i in 1:length(BiocManager.set)) {
+      if (!requireNamespace(BiocManager.set[i], quietly = TRUE)){
+        BiocManager::install(BiocManager.set[i])
+      }
+    }
+    ## Load Packages
+    lapply(BiocManager.set, library, character.only = TRUE)
+    rm(BiocManager.set,i)
+
+  }
 
 }
