2331f323cc6982c8396bbe429994d7b2aecfd62d
Charlene <q2330416@gmail.com>
2023-02-04 12:12:58 +0800

    Change objects' name

--- Sup_FrmatConversion.R
+++ Sup_FrmatConversion.R
@@ -3,31 +3,31 @@ rm(list = ls()) # Clean variable
 memory.limit(150000)
 
 ##### Load Packages #####
-  if(!require("tidyverse")) install.packages("tidyverse")
-  library(tidyverse)
+if(!require("tidyverse")) install.packages("tidyverse")
+library(tidyverse)
 
 
 ##### Import setting and Import* #####
-  ## File setting*
-  InFOLName_GE <- "Input_TCGA"  # Input Folder Name
-  SampleName <- "Xena_TCGA_LGG_GE"
-  SamplePhenoName <- "TCGA.LGG.sampleMap_LGG_clinicalMatrix"
+## Import setting*
+InFOLName_GE <- "Input_TCGA"  # Input Folder Name
+FileName_GE <- "Xena_TCGA_LGG_GE"
+FileName_Pheno <- "TCGA.LGG.sampleMap_LGG_clinicalMatrix"
 
-  ## Import genetic data file
-  GeneExp.df <- read.table(paste0(InFOLName_GE,"/",SampleName), header=T, row.names = 1, sep="\t")
-  Anno.df <- read.table(paste0(InFOLName_GE,"/",SamplePhenoName), header=T, row.names = 1, sep="\t")
+## Import file
+GeneExp.df <- read.table(paste0(InFOLName_GE,"/",FileName_GE), header=T, row.names = 1, sep="\t")
+Anno.df <- read.table(paste0(InFOLName_GE,"/",FileName_Pheno), header=T, row.names = 1, sep="\t")
 
 ##### Export #####
-  ## Export tsv files
-  GeneExp.df <- data.frame(Gene=row.names(GeneExp.df),GeneExp.df)
-  write.table(GeneExp.df,paste0(InFOLName_GE,"/",SampleName,".tsv"), col.names =T, row.names = F, sep="\t", quote = F)
+## Export tsv files
+GeneExp.df <- data.frame(Gene=row.names(GeneExp.df),GeneExp.df)
+write.table(GeneExp.df,paste0(InFOLName_GE,"/",FileName_GE,".tsv"), col.names =T, row.names = F, sep="\t", quote = F)
 
-  Anno.df <- data.frame(Gene=row.names(Anno.df),Anno.df)
-  write.table(Anno.df,paste0(InFOLName_GE,"/",SamplePhenoName,".tsv"), col.names =T, row.names = F, sep="\t", quote = F)
+Anno.df <- data.frame(Gene=row.names(Anno.df),Anno.df)
+write.table(Anno.df,paste0(InFOLName_GE,"/",FileName_Pheno,".tsv"), col.names =T, row.names = F, sep="\t", quote = F)
 
-  ## Create small dataset for test
-  GeneExpS.df <- GeneExp.df[,sample(ncol(GeneExp.df),100)]
-  write.table(GeneExpS.df,paste0(InFOLName_GE,"/",SampleName,"_S.tsv"), col.names =T, row.names = F, sep="\t", quote = F)
+## Create small dataset for test
+GeneExpS.df <- GeneExp.df[,sample(ncol(GeneExp.df),100)]
+write.table(GeneExpS.df,paste0(InFOLName_GE,"/",FileName_GE,"_S.tsv"), col.names =T, row.names = F, sep="\t", quote = F)
 
 
 
