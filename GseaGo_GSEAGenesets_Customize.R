0359649fb12046da5a023850e4e7986f4afeb920
Charlene <q2330416@gmail.com>
2023-02-06 19:02:00 +0800

    Try msigdf

--- GseaGo_GSEAGenesets_Customize.R
+++ GseaGo_GSEAGenesets_Customize.R
@@ -10,8 +10,8 @@
   library(tidyverse)
   if(!require("gtools")) install.packages("gtools")
   library(gtools)
-  if(!require("XML")) install.packages("XML")
-  library(XML)
+  # if(!require("XML")) install.packages("XML")
+  # library(XML)
 
 ##### Condition setting* #####
   SpeciesSet = "Homo sapiens"
@@ -38,9 +38,14 @@
 
 ##### Import files & Combine df #####
   ## Import all from GSEA GeneSet
-  # Ref: https://www.educative.io/answers/how-to-read-xml-files-in-r
-  GSEAGeneSet.df <- xmlToDataFrame(paste0("Input_Genesets/Gsea_Genesets_Hs/msigdb_v2022.1.Hs.xml"))
-  GSEAGeneSet.df <- read.delim2(paste0("Input_Genesets/Gsea_Genesets_Hs/msigdb_v2022.1.Hs.xml"),sep = "\t")
+  # # Ref: https://www.educative.io/answers/how-to-read-xml-files-in-r
+  # GSEAGeneSet.df <- xmlToDataFrame(paste0("Input_Genesets/Gsea_Genesets_Hs/msigdb_v2022.1.Hs.xml"))
+  # GSEAGeneSet.df <- read.delim2(paste0("Input_Genesets/Gsea_Genesets_Hs/msigdb_v2022.1.Hs.xml"),sep = "\t")
+
+  # Ref: https://stephenturner.github.io/msigdf/vignettes/msigdf
+  if (!require("BiocManager", quietly = TRUE)) install.packages("BiocManager")
+  if(!require("msigdf")) BiocManager::install("msigdf")
+  library(msigdf)
 
   ## Import Customization
   # target.dir <- list.dirs( paste0("Input_Genesets/", InputFolder) )[-1]
